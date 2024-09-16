45e9323f57381568d6e6a703e0958759cf9d9c78
Carolina Lennon <14179052+lennon-c@users.noreply.github.com>
2024-09-16 09:49:25 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='shapes',
+   version="0.1",
+   description='An example package built using conda',
+   author='carolina lennon (codio)',
+   author_email='carolina.lennon@gmail.com',
+   packages=['shapes'],  
+   install_requires=[],
+)
